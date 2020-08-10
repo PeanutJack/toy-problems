4d4ef611c2664402a2e9dd51bab2df1ddd4a12f4
Robert Bessler <peanutjackreigns@gmail.com>
2020-08-10 17:21:34 -0400

    Improves speed of excel-sheet-number using object instead of array

--- aug2020/excel-sheet-number.js
+++ aug2020/excel-sheet-number.js
@@ -33,23 +33,43 @@
 // s consists only of uppercase English letters.
 // s is between "A" and "FXSHRXW".
 
-var excelColumnNumber = function(str) {
-  var output = 0;
-  var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  for (var i = 1; i < str.length + 1; i++) {
-    var letter = str[str.length - i];
-    var letterVal = letters.indexOf(letter) + 1;
-    var add = (26 ** (i - 1) * letterVal);
-    if (i === 1) {
-      output = letterVal;
-    } else {
-      output += add;
-    }
+var titleToNumber = function(str) {
+  var letters = {
+    A: 1,
+    B: 2,
+    C: 3,
+    D: 4,
+    E: 5,
+    F: 6,
+    G: 7,
+    H: 8,
+    I: 9,
+    J: 10,
+    K: 11,
+    L: 12,
+    M: 13,
+    N: 14,
+    O: 15,
+    P: 16,
+    Q: 17,
+    R: 18,
+    S: 19,
+    T: 20,
+    U: 21,
+    V: 22,
+    W: 23,
+    X: 24,
+    Y: 25,
+    Z: 26
+  };
+  var output = letters[str[str.length - 1]];
+  for (var i = 2; i < str.length + 1; i++) {
+    output += (26 ** (i - 1) * letters[str[str.length - i]]);
   }
   return output;
 };
 
-console.log(excelColumnNumber('A'));   // 1
-console.log(excelColumnNumber('AB'));  // 28
-console.log(excelColumnNumber('ZY'));  // 701
-console.log(excelColumnNumber('ABC')); // 731
+console.log(titleToNumber('A'));   // 1
+console.log(titleToNumber('AB'));  // 28
+console.log(titleToNumber('ZY'));  // 701
+console.log(titleToNumber('ABC')); // 731
