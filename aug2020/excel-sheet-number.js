b5f5c7705e951c0420ac92463e9f8f66e9aac9ce
Robert Bessler <peanutjackreigns@gmail.com>
2020-08-10 13:00:51 -0400

    Completes excel-sheet-number prompt

---
+++ aug2020/excel-sheet-number.js
@@ -0,0 +1,54 @@
+// Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+// For example:
+
+//     A -> 1
+//     B -> 2
+//     C -> 3
+//     ...
+//     Z -> 26
+//     AA -> 27
+//     AB -> 28
+//     ...
+
+// Example 1:
+
+// Input: "A"
+// Output: 1
+
+// Example 2:
+
+// Input: "AB"
+// Output: 28
+
+// Example 3:
+
+// Input: "ZY"
+// Output: 701
+
+
+// Constraints:
+
+// 1 <= s.length <= 7
+// s consists only of uppercase English letters.
+// s is between "A" and "FXSHRXW".
+
+var excelColumnNumber = function(str) {
+  var output = 0;
+  var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (var i = 1; i < str.length + 1; i++) {
+    var letter = str[str.length - i];
+    var letterVal = letters.indexOf(letter) + 1;
+    var add = (26 * (i - 1) * letterVal);
+    if (i === 1) {
+      output = letterVal;
+    } else {
+      output += add;
+    }
+  }
+  return output;
+};
+
+console.log(excelColumnNumber('A'));
+console.log(excelColumnNumber('AB'));
+console.log(excelColumnNumber('ZY'));
