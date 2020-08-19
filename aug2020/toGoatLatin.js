99c0f04cd4a21c2b8c9a6deaed4698de4027092a
Robert Bessler <peanutjackreigns@gmail.com>
2020-08-19 10:06:40 -0400

    Reduces toGoatLatin to very few lines

--- aug2020/toGoatLatin.js
+++ aug2020/toGoatLatin.js
@@ -3,16 +3,16 @@
  * @return {string}
  */
 var toGoatLatin = function(S) {
-  const vowels = ['a', 'e', 'i', 'o', 'u'];
-  let words = S.split(' ');
-  for (let i = 0; i < words.length; i++) {
-    // if the first letter is not a vowel
-    if (vowels.indexOf(words[i][0].toLowerCase()) === -1) {
-      words[i] = words[i].slice(1) + words[i][0];
-    }
-    words[i] += 'ma' + 'a'.repeat(i + 1);
-  }
-  return words.join(' ');
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  // let words = S.split(' ');
+  // for (let i = 0; i < words.length; i++) {
+  //   // if the first letter is not a vowel
+  //   if (vowels.indexOf(words[i][0].toLowerCase()) === -1) {
+  //     words[i] = words[i].slice(1) + words[i][0];
+  //   }
+  //   words[i] += 'ma' + 'a'.repeat(i + 1);
+  // }
+  return S.split(' ').map((w, i) => (vowels.includes(w[0]) ? w : w.slice(1) + w[0]) + 'ma' + 'a'.repeat(i + 1)).join(' ');
 };
 
 console.log(toGoatLatin('I speak Goat Latin'));
