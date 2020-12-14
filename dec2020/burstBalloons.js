d09ba427501f3c1f35d7a64137fc4478b9f241fe
Robert Bessler <peanutjackreigns@gmail.com>
2020-12-14 01:08:24 -0800

    Attempts burstBalloons again

--- dec2020/burstBalloons.js
+++ dec2020/burstBalloons.js
@@ -21,30 +21,35 @@ var maxCoins = function(nums) {
   if (nums.length === 0) return 0;
   // going to go for super long time complexity
   let output = 0;
-  let min = Infinity;
-  let minInd = 0;
+  let max = -Infinity;
+  let maxInd = 0;
   while(nums.length > 2) {
-    // iterate over nums except for edges to find min
+    // iterate over nums except for edges to find max adjacent product
     for (let i = 1; i < nums.length - 1; i++) {
-      if (nums[i] < min) {
-        min = nums[i];
-        minInd = i;
+      if (nums[i - 1] * nums[i + 1] > max) {
+        max = nums[i - 1] * nums[i + 1];
+        maxInd = i;
       }
     }
     console.log(nums, output)
-    output += nums[minInd - 1] * nums[minInd] * nums[minInd + 1];
-    nums.splice(minInd, 1);
-    min = Infinity;
-    minInd = 0;
+    output += nums[maxInd - 1] * nums[maxInd] * nums[maxInd + 1];
+    nums.splice(maxInd, 1);
+    max = -Infinity;
+    maxInd = 0;
   }
   output += nums[0] * nums[1];
   output += Math.max(nums[0], nums[1]);
   return output;
 };
 
-console.log(maxCoins([9,76,64,21,97,60]));
+// console.log(maxCoins([9,76,64,21,97,60]));
 // this does not pass because when there are 4 items left [9,76,97,60] the optimal selection is 97
   // I don't know how to come to that conclusion from a blanket statement
   // I KNOW that you have to take the edges last
   // It's not "the lower/higher edge"
   // it MIGHT be some special calculations when there are 4 things left, but my blanket of taking the smallest might also be wrong for other tests
+
+console.log(maxCoins([35,16,83,87,84,59,48,41,20])); // 1611332
+// changed algorithm to instead of picking min, it picks whichever has greatest product of adjacent numbers
+  // passed previous test, but now fails this one
+  // REALLY not sure where this one fails, I'm only off by about 22k out of 1.6M
