4eb9e54637f476af3a1f6377daecdd02cb1ccf73
Robert Bessler <peanutjackreigns@gmail.com>
2020-12-16 23:42:17 -0800

    Completes validateBST

---
+++ dec2020/validateBST.js
@@ -0,0 +1,20 @@
+// Strat: put everything into array then check if array is ascending
+// Better strat (better space and allows early escape) : set limits as I descend down
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root, min = -Infinity, max = Infinity) {
+  if (!root) return true;
+  if (root.val >= max || root.val <= min) return false;
+  return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+};
